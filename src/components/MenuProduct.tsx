597ebccdaf8ee8a8a1677e8f897eba68643f5ad6
AndresMGDev <cmendoza022002@gmail.com>
2024-09-16 00:03:34 -0500

    permision

--- src/components/MenuProduct.tsx
+++ src/components/MenuProduct.tsx
@@ -10,7 +10,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 export const MenuPhone = () => {
 
     const [countPro, setCountPro] = useState(0);
-    const [email, setEmail] = useState<string | null>(null);
+    const [email, setEmail] = useState<boolean | null>(null);
 
     useEffect(() => {
         const getProducts = async () => {
@@ -33,12 +33,12 @@ export const MenuPhone = () => {
       if (user && user.email) {
         const domain = user.email.split('@')[1]; // Extrae el dominio del email
         if (domain === 'adrenalinadmin.com') {
-          setEmail(user.email); // Solo muestra el email si es del dominio @adrenalinadmin.com
+          setEmail(true); // Solo muestra el email si es del dominio @adrenalinadmin.com
         } else {
-          setEmail(null); // No muestra el email si es de otro dominio
+          setEmail(false); // No muestra el email si es de otro dominio
         }
       } else {
-        setEmail(null); // No hay usuario autenticado
+        setEmail(false); // No hay usuario autenticado
       }
     });
 
@@ -76,11 +76,11 @@ export const MenuPhone = () => {
                                 <path d="M896 1537V936L256 616v880l544 273l-31 127l-641-320V472L960 57l832 415v270q-70 11-128 45V616l-640 320v473zM754 302l584 334l247-124l-625-313zm206 523l240-120l-584-334l-281 141zm888 71q42 0 78 15t64 41t42 63t16 79q0 39-15 76t-43 65l-717 717l-377 94l94-377l717-716q29-29 65-43t76-14m51 249q21-21 21-51q0-31-20-50t-52-20q-14 0-27 4t-23 15l-692 692l-34 135l135-34z" />
                             </g>
                         </svg>
-                        Ventas
+                        Ventas {email}
                     </Link>
                 </li>
                 {
-                    email == 'adrenalinadmin.com' ? <li>
+                    email == true ? <li>
                     <Link href="/product/admin" className={`${pathname === '/product' ? 'active' : ''}`}>
                     <svg xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
